de6849c7401bcd138092fc6e4039386c1d926ede
Claire Bullock <bullockcm13@gmail.com>
2020-02-17 16:01:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,1 @@
 def square_array(array)
-  square_array = [1, 2, 3]
-  numbers.each do |square_array|
-    square_array << (square_array**2)
-    return square_array
-  end
-  
-  # your code here
-end
